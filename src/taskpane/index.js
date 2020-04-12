69ce2a93da986aebdafb6e7e1ce2301eb50d38c8
gignacy <itjaniszewski@gmail.com>
2020-04-12 16:27:20 +0200

    Comparare inserted from localhost

--- src/taskpane/index.js
+++ src/taskpane/index.js
@@ -1,10 +1,8 @@
 import "office-ui-fabric-react/dist/css/fabric.min.css";
-import App from "./components/App";
-import { AppContainer } from "react-hot-loader";
 import { initializeIcons } from "office-ui-fabric-react/lib/Icons";
 import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
 import { List } from 'office-ui-fabric-react/lib/List';
-import { createListItems, IExampleItem } from '@uifabric/example-data';
+import { Selection } from 'office-ui-fabric-react/lib/DetailsList';
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { mergeStyleSets, getTheme, normalize } from 'office-ui-fabric-react/lib/Styling';
@@ -52,67 +50,93 @@ class SearchBoxClass extends React.Component {
     }
 
     updateState(resultsList) {
-        // Note: this will *not* work as intended.
         this.props.setParentState(resultsList)
     }
-
 
       render() {
 
         return (
-            <div className="searchbox">
-              <SearchBox
-                  placeholder="tutaj cos wpisz"
-                  // onSearch={newValue => console.log('value is ' + newValue)}
-                  onChange={(i, newValue) =>
-                  {
-                      var resultsObj = {};
-                      var resultsList = [];
-                      axios.get(`https://simplify-docs.appspot.com/krs/search/${newValue}`)
-                          .then(function (response) {
-                              var data = response['data'].results;
-                              for (var result in data) {
-                                  resultsObj["name"] = data[result].name
-                                  resultsList.push(resultsObj)
-                                  resultsObj = {};
-                              }
-                          })
-                          .catch(function (error) {
-                              // handle error
-                              // console.log(error);
-                          })
-                          .then(function () {
-                          });
-                      this.updateState(resultsList)
-                  }}
-
-                  // onFocus={() => console.log(`onFocus called`)}
-                  // onBlur={() => console.log(`onBlur called`)}
-                  underlined={true}
-              />
-            </div>
-        );
-      }
+          <div className="searchbox">
+            <SearchBox
+                placeholder="tutaj cos wpisz"
+                onChange={(i, newValue) =>
+                {
+                    var resultsObj = {};
+                    var resultsList = [];
+                    axios.get(`https://simplify-docs.appspot.com/krs/search/${newValue}`)
+                        .then(function (response) {
+                            var data = response['data'].results;
+                            for (var result in data) {
+                                resultsObj["name"] = data[result].name
+                                resultsObj["krs"] = data[result].krs
+                                resultsList.push(resultsObj)
+                                resultsObj = {};
+                            }
+                        })
+                        .catch(function (error) {
+                            // handle error
+                            // console.log(error);
+                        })
+                        .then(function () {
+                        });
+                    this.updateState(resultsList)
+                }}
+                underlined={true}
+            />
+          </div>
+      );
     }
-
+  }
 
 
 class ResultsList extends React.Component {
       constructor(props){
           super(props);
-    }
+
+      this._selection = new Selection({
+        onSelectionChanged: () => this.setState({ selectionDetails: this._getSelectionDetails() }),
+        })
+      }
+
+      click = (event) => {
+        const listIndex = event.target.dataset.listIndex;
+        const text = this.props.searchValue[listIndex];
+        const krsNo = text.krs
+        console.log(krsNo)
+        this.getComparareByKrsNo(krsNo)
+      }
+
+      getComparareByKrsNo = (krsNo) => {
+        axios.get(`http://localhost:5000/get_krs_data/${krsNo}`)
+            .then(function (response) {
+              var comparare = response['data'].results;
+              console.log(comparare);
+              return Word.run(async context => {
+                // insert a paragraph at the end of the document.
+                const paragraph = context.document.body.insertParagraph(String(comparare), Word.InsertLocation.end);
+                // change the paragraph color to blue.
+                paragraph.font.color = "black";
+                await context.sync();
+              })
+            })
+            .catch(function (error) {
+                // handle error
+                // console.log(error);
+            })
+            // Will execute regardless of the result of the function
+            .then(function () {
+            });
+      }
+
 
       render() {
-
         return (
-
             <div className={styles.container} data-is-scrollable={true}>
               <List
                   // ref={this._resolveList}
                   items={this.props.searchValue}
-                  //
-                  // getPageHeight={this._getPageHeight}
-                  // onRenderCell={this._onRenderCell}
+                  onClick={event => this.click(event)}
+                  onSelect={() => console.log(`onSelect called`)}
               />
             </div>
         );
@@ -160,4 +184,3 @@ ReactDOM.render(
 <AddinComponent />,
     document.getElementById('container')
 );
-
